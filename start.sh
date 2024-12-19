cc76b358a26b82030ba11372f3bfa8154e32d637
Nastya Stepanova <Nastya.stepanova1104@yandex.ru>
2024-12-19 14:59:31 +0300

    for glitch

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Exit early on errors
+curl https://pyenv.run | bash
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+pyenv install 3.8
 set -eu
 
 # Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
@@ -18,6 +23,7 @@ if [ ! -f $VIRTUALENV/bin/pip ]; then
 curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
+$VIRTUALENV/bin/python3 -m pip install -U pip
 # Install the requirements
 $VIRTUALENV/bin/pip install -r requirements.txt
 
